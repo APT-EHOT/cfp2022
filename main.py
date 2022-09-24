9279713ef8c855029e3582b010337c3059fc3831
Alexey Antipin <alexey@kittco.ru>
2022-09-24 20:09:49 +0300

    Boabs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+from datetime import datetime
 
 from catboost.datasets import titanic
 from sklearn.model_selection import train_test_split
@@ -9,50 +10,43 @@ import pandas as pd
 
 
 def main():
-    train_df, test_df = titanic()
+    train_df = pd.read_csv("result.csv")
+    #test_df = pd.read_csv("test.csv")
     train_df.head()
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(train_df)
-    print(test_df)
+    #print(test_df)
 
-    null_value_stats = train_df.isnull().sum(axis=0)
-    null_value_stats[null_value_stats != 0]
+    for item in train_df:
+        del item[1]
 
-    train_df.fillna(-999, inplace=True)
-    test_df.fillna(-999, inplace=True)
-    X = train_df.drop('Survived', axis=1)
-    y = train_df.Survived
-    print(train_df.Survived)
-    print(X.dtypes)
+    X = train_df.drop('currentprice', axis=1)
+    y = train_df.currentprice
 
-    categorical_features_indices = np.where(X.dtypes != float)[0]
-    X_train, X_validation, y_train, y_validation = train_test_split(X, y, train_size=0.75, random_state=42)
+    categorical_features_indices = np.where(X.dtypes != datetime)[0]
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, train_size=0.75)
 
-    model = CatBoostClassifier(
-        custom_loss=[metrics.Accuracy()],
-        random_seed=42,
-        logging_level='Silent'
-    )
+    model = CatBoostClassifier(custom_loss=[metrics.Accuracy()], iterations=10, logging_level='Silent')
     model.fit(
         X_train, y_train,
         cat_features=categorical_features_indices,
         eval_set=(X_validation, y_validation)
     )
-    cv_params = model.get_params()
-    cv_params.update({
-        'loss_function': metrics.Logloss()
-    })
-    cv_data = cv(
-        Pool(X, y, cat_features=categorical_features_indices),
-        cv_params
-    )
-    print('Best validation accuracy score: {:.2f}±{:.2f} on step {}'.format(
-        np.max(cv_data['test-Accuracy-mean']),
-        cv_data['test-Accuracy-std'][np.argmax(cv_data['test-Accuracy-mean'])],
-        np.argmax(cv_data['test-Accuracy-mean'])
-    ))
-    print('Precise validation accuracy score: {}'.format(np.max(cv_data['test-Accuracy-mean'])))
+    #cv_params = model.get_params()
+    #cv_params.update({
+    #    'loss_function': metrics.Logloss()
+    #})
+    #cv_data = cv(
+    #    Pool(X, y, cat_features=categorical_features_indices),
+    #    cv_params
+    #)
+    #print('Best validation accuracy score: {:.2f}±{:.2f} on step {}'.format(
+    #    np.max(cv_data['test-Accuracy-mean']),
+    #    cv_data['test-Accuracy-std'][np.argmax(cv_data['test-Accuracy-mean'])],
+    #    np.argmax(cv_data['test-Accuracy-mean'])
+    #))
+    #print('Precise validation accuracy score: {}'.format(np.max(cv_data['test-Accuracy-mean'])))
 
 
 main()
