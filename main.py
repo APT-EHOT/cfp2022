da1b897d76eb395bd6ab945f5e90b9eec39df432
Alexey Antipin <alexey@kittco.ru>
2022-09-25 10:25:57 +0300

    test8

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 def main():
-    train_df = pd.read_csv("predict7.csv")
+    train_df = pd.read_csv("predict8.csv")
 
     model = CatBoostRegressor()
     model.load_model('catboost_model2.dump')
